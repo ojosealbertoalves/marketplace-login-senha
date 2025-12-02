8f086c6e3791f04204091e975802e649ef698b33
Jose Alberto Alves <alveslemosneto@gmail.com>
2025-12-02 08:51:28 -0300

    try deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js - COM AXIOS E FETCH
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';;
 
 // ===== CONFIGURAÇÃO DO AXIOS =====
 const axiosInstance = axios.create({
